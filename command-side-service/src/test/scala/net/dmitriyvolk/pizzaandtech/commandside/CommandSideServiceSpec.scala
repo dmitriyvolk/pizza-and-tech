75d583ed6bbd6a83e3f64a9c5c31aef5108600f4
Dmitriy Volk <dmitriy@dmitriyvolk.net>
2015-11-11 13:10:41 -0800

    Test not working due to RegistyMetric dependency missing.
    Committing and tagging for future investigation

--- command-side-service/src/test/scala/net/dmitriyvolk/pizzaandtech/commandside/CommandSideServiceSpec.scala
+++ command-side-service/src/test/scala/net/dmitriyvolk/pizzaandtech/commandside/CommandSideServiceSpec.scala
@@ -1,11 +1,28 @@
 package net.dmitriyvolk.pizzaandtech.commandside
 
 import net.dmitriyvolk.pizzaandtech.commandside.configuration.CommandSideServiceTestConfiguration
-import org.scalatest.{Matchers, WordSpec}
+import net.dmitriyvolk.pizzaandtech.commandside.web.CreateGroupResponse
+import net.dmitriyvolk.pizzaandtech.domain.group.GroupDetails
+import org.scalatest.{GivenWhenThen, FeatureSpec, Matchers}
 import org.springframework.boot.SpringApplication
+import org.springframework.web.client.RestTemplate
 
-class CommandSideServiceSpec extends WordSpec with Matchers {
+class CommandSideServiceSpec extends FeatureSpec with GivenWhenThen with Matchers {
 
   val ctx = new SpringApplication(classOf[CommandSideServiceTestConfiguration]).run()
+  val restTemplate = ctx.getBean(classOf[RestTemplate])
+  val port = 8080
+  val baseUrl = s"http://localhost:$port"
+  val groupsUrl = s"$baseUrl/groups"
+
+  feature("A group can be created") {
+    scenario("User can register a group with complete and valid information") {
+      When("API is invoked")
+      val groupDetails = GroupDetails(name = "new group", description = "new group description")
+      val response = restTemplate.postForEntity[CreateGroupResponse](groupsUrl, groupDetails, classOf[CreateGroupResponse])
+      Then("a new GroupId should be returned")
+      response.getBody().groupId should not be null
+    }
+  }
 
 }
--- command-side-service/src/test/scala/net/dmitriyvolk/pizzaandtech/commandside/configuration/CommandSideServiceTestConfiguration.scala
+++ command-side-service/src/test/scala/net/dmitriyvolk/pizzaandtech/commandside/configuration/CommandSideServiceTestConfiguration.scala
@@ -4,6 +4,7 @@ import net.chrisrichardson.eventstore.json.EventStoreCommonObjectMapping
 import org.springframework.context.annotation.{Bean, Import, Configuration}
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import scala.collection.JavaConversions._
 
 @Configuration
 @Import(Array(classOf[CommandSideConfiguration]))
@@ -12,7 +13,7 @@ class CommandSideServiceTestConfiguration {
   @Bean
   def restTemplate() = {
     val restTemplate = new RestTemplate()
-    restTemplate.getMessageConverters forEach {
+    restTemplate.getMessageConverters foreach {
       case mc: MappingJackson2HttpMessageConverter =>
         mc.setObjectMapper(EventStoreCommonObjectMapping.getObjectMapper)
       case _ =>
